59242947ad7971fae4487eb622547de4775c2750
Sam Clark <samuel.clark@stonybrook.edu>
2018-01-27 03:12:39 -0500

    Removed .files

--- classes/Error.js
+++
@@ -1,9 +0,0 @@
-const WSMessage = require('./WSMessage.js');
-
-module.exports = class Error extends WSMessage {
-	constructor(text) {
-		super('error', {
-			text: text
-		});
-	}
-};
--- classes/Message.js
+++
@@ -1,7 +0,0 @@
-module.exports = class {
-	constructor(sender, thread, content) {
-		this.sender = sender; // User ID
-		this.thread = thread; // Thread ID
-		this.content = content;
-	}
-};
