cc81f8260ded3f0271a2e8daaa32a35be23596da
Sam Clark <samuel.clark@stonybrook.edu>
2018-01-27 19:34:02 -0500

    Release

--- client/notificationsSW.js
+++ client/notificationsSW.js
@@ -1,4 +1,17 @@
 // TODO add push services
+self.addEventListener('install', function(event) {
+	// The promise that skipWaiting() returns can be safely ignored.
+	self.skipWaiting();
+
+	// Perform any other actions required for your
+	// service worker to install, potentially inside
+	// of event.waitUntil();
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(clients.claim());
+});
+
 self.addEventListener('push', function(e) {
 	var options = {
 		body: e.data.text(),
