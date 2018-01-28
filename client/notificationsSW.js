9f2872f42ba40dcc2cfb510f3896ab920a76e2b0
Sam Clark <samuel.clark@stonybrook.edu>
2018-01-27 19:32:03 -0500

    Release

--- client/notificationsSW.js
+++ client/notificationsSW.js
@@ -5,10 +5,37 @@ self.addEventListener('push', function(e) {
 		vibrate: [100, 50, 100],
 		data: {
 			dateOfArrival: Date.now(),
-			primaryKey: '2'
+			primaryKey: '2',
+			tag: 'renotify',
+			renotify: true
 		}
 	};
 	e.waitUntil(
 		self.registration.showNotification('PeepChat', options)
 	);
+});
+
+self.addEventListener('notificationclick', function(e) {
+	const clickedNotification = e.notification;
+	clickedNotification.close();
+
+	e.waitUntil(
+		clients.matchAll({
+			type: 'window'
+		})
+			.then(function(clientList) {
+				for (var i = 0; i < clientList.length; i++) {
+					var client = clientList[i];
+					if ('focus' in client)
+						return client.focus();
+				}
+				if (clients.openWindow) {
+					return clients.openWindow(e.currentTarget.location.origin);
+				}
+			})
+	);
+
+	// Do something as the result of the notification click
+	/*const promiseChain =
+	e.waitUntil(promiseChain);*/
 });
